576212bb50d812fc07695d4883a128bcebc019b3
MrVid <102604076+MrVidMM@users.noreply.github.com>
2022-12-12 12:41:31 -0300

    Reglas

---
+++ 05Reglas.js
@@ -0,0 +1,27 @@
+// Reglas para definir nombres de Variables en JavaScript
+
+// ahora veremos que javascript es sensible a mayusculas
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Lara";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+// podemos darnos cuenta que uno con min y otro con may
+// pueden verse como dos variables diferentes, por eso
+// tenemos que tener mucho cuidado con nuestras definiciones
+
+// las variables no pueden definirse con un numero al inicio
+// descomentalo para ver!
+//let 1nombreCompleto;
+
+// pero si podemos con guión bajo (_) o dolar ($)
+// y tambien numero pero solo despues de letras o las anteriores
+let nombre$_Completo1;
+
+// let 1nombreCompleto; no esta permitido 
+
+/* Tambien saber que si existe una palabra reservada
+   no te permitara definirla como variable
+   por ejemplo: let break; intentalo
+*/
+
