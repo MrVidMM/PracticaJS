fb1a09852ebbfb9080609cfc20a97ce03c920991
MrVid <102604076+MrVidMM@users.noreply.github.com>
2022-12-12 12:20:12 -0300

    var let y const agregados

---
+++ 03var_let_const.js
@@ -0,0 +1,24 @@
+/**
+ * let: agregamos una variable
+ * const: agregamos una constante
+ * 
+ * pd: las const sirven para que las variables
+ *     luego que les asignamos un valor a una
+ *     variable no se puedan modificar esos valores
+ */
+
+let nombre = "Juan";
+console.log(nombre);
+
+const apellido = "Perez";
+// Si nos fijamos no nos permitiran asignar un nuevo valor
+// descomenta el codigo para verlo
+// apellido = "Lara";
+
+/**
+ * Por ende utilizaremos "let" para asignar variables
+ * que cambien y "const" cuando no queremos que el valor cambie
+ */
+
+// Vamos a ponerlo en practica...
+
---
+++ 04let_const.js
@@ -0,0 +1,13 @@
+/* Para buenas practicas utilizaremos notación de altasemantica
+   en este caso si la variable esta formada por 2 palabras
+   la primera palabra ira en minuscula y la primera letra de la segunda
+   palabra tendra que estar en mayuscula */
+
+let nombreCompleto = "Juan Perez";
+console.log(nombreCompleto);
+
+// podemos declarar más variables en una sola linea
+let x, y; // definimos "x,y"
+x = 10, y = 20; // Asignamos un valor para cada variable
+let z = x + y; // definimos otra variable "z" con la suma de "x + y"
+console.log(z); // mostramos el valor que es =
